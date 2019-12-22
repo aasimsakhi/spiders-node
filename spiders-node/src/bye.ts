a715c23132580dbb294d6a8a2e8c516486b71482
asim <you@example.com>
2019-12-22 11:54:27 -0600

    work completed

---
+++ spiders-node/src/bye.ts
@@ -0,0 +1,11 @@
+export const bye: any= async (event, _context) => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+        name:'Asim',
+        input: event,
+      }, null, 2),
+    };
+  }
+  
